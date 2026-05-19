0500552082be7b2406af863e4b9f9db4ecaf56ea
Shafayat Islam <shafayatislamsaad@gmail.com>
2026-05-19 11:07:55 +0600

    Add UserNotFoundException

---
+++ habit_management/src/main/java/com/project/habit_management/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.habit_management.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Integer userId){
+        super("User with ID: "+userId+" not found!!");
+    }
+}
