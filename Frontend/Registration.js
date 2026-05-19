5049e0baedb568c14f65f783aa5a789e0d14dfa1
Shafayat Islam <shafayatislamsaad@gmail.com>
2026-05-19 11:35:25 +0600

    Connect Registration page to database using API

--- Frontend/Registration.js
+++ Frontend/Registration.js
@@ -1,5 +1,6 @@
+const url = "http://localhost:8080/users/registration";
+
 const form = document.querySelector(".page-form");
-const inputs = form.querySelectorAll("input, select");
 
 const passwordInput = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm");
@@ -9,10 +10,6 @@ const strengthText = document.getElementById("strength-text");
 
 const strength_bars = password_strength.querySelectorAll("span");
 const strength_text = document.getElementById("strength-text");
-
-strength_bars.forEach(bar => {
-    console.dir(bar);
-});
 
 passwordInput.addEventListener("input", ()=>{
     const password = passwordInput.value.trim();
@@ -61,3 +58,84 @@ function disableStrengthBars(){
 
     strength_text.classList = "strength-text";
 }
+
+form.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+
+    const inputs = form.querySelectorAll("input, select");
+
+    const password = inputs[7].value.trim();
+    const confirmPassword = inputs[8].value.trim();
+    console.log("password: ",password," confirm password: ",confirmPassword);
+
+    if(password !== confirmPassword){
+        let overlay = document.getElementById('overlay');
+        let popup = document.getElementById('password-confirmation');
+        overlay.classList.replace("hidden", "overlay");
+        popup.classList.replace("hidden", "popup");
+
+        let button = popup.querySelector("button");
+        button.addEventListener("click", () => {
+            overlay.classList.replace("overlay", "hidden");
+            popup.classList.replace("popup", "hidden");
+        });
+    }
+
+    const userData = {
+        fullName: (inputs[0].value+" "+inputs[1].value).trim(),
+        email: inputs[2].value.trim(),
+        dateOfBirth: inputs[3].value,
+        gender: inputs[4].value,
+        address: inputs[5].value.trim(),
+        username: inputs[6].value.trim(),
+        password: password
+    }
+
+    console.log(userData);
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    });
+
+    if(!response.ok){
+        const error = await response.json();
+        
+        let overlay = document.getElementById('overlay');
+        let popup = document.getElementById('error-alert');
+        overlay.classList.replace("hidden", "overlay");
+        popup.classList.replace("hidden", "popup");
+
+        if(error.status === 111){
+            popup.querySelector("p").textContent = error.message;
+        }
+
+        let button = popup.querySelector("button");
+        button.addEventListener("click", () => {
+            overlay.classList.replace("overlay", "hidden");
+            popup.classList.replace("popup", "hidden");
+        });
+
+        return;
+    }
+
+    let overlay = document.getElementById('overlay');
+    let popup = document.getElementById('reg-success');
+    overlay.classList.replace("hidden", "overlay");
+    popup.classList.replace("hidden", "popup");
+
+    let cancel_btn = document.getElementById('cancel-btn');
+    let go_to_login_btn = document.getElementById('go-to-login-page');
+
+    cancel_btn.addEventListener("click", () => {
+        overlay.classList.replace("overlay", "hidden");
+        popup.classList.replace("popup", "hidden");
+    });
+
+    go_to_login_btn.addEventListener("click", () => {
+        window.location.href = "";
+    });
+});
