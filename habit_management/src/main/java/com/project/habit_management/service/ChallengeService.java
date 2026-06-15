f33443b9ba3e183cca82387e9ee2a71426016981
Shafayat Islam <shafayatislamsaad@gmail.com>
2026-06-15 17:21:03 +0600

    add challenge services

---
+++ habit_management/src/main/java/com/project/habit_management/service/ChallengeService.java
@@ -0,0 +1,123 @@
+package com.project.habit_management.service;
+
+import com.project.habit_management.dto.ChallengeRequest;
+import com.project.habit_management.dto.ChallengeResponse;
+import com.project.habit_management.exception.UserNotFoundException;
+import com.project.habit_management.model.*;
+import com.project.habit_management.repository.ChallengeRepo;
+import com.project.habit_management.repository.ChallengeSecurityRepo;
+import com.project.habit_management.repository.UserChallengeRepo;
+import com.project.habit_management.repository.UserRepo;
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class ChallengeService {
+    final private ChallengeRepo challengeRepo;
+    final private UserRepo userRepo;
+    final private ChallengeSecurityRepo securityRepo;
+    final private UserChallengeRepo userChallengeRepo;
+    final private PasswordEncoder passwordEncoder;
+
+    public ChallengeService(ChallengeRepo challengeRepo, UserRepo userRepo, ChallengeSecurityRepo securityRepo, UserChallengeRepo userChallengeRepo, PasswordEncoder passwordEncoder) {
+        this.challengeRepo = challengeRepo;
+        this.userRepo = userRepo;
+        this.securityRepo = securityRepo;
+        this.userChallengeRepo = userChallengeRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void createChallenge(ChallengeRequest request){
+        User user = userRepo.findById(request.getUserId()).orElseThrow(UserNotFoundException::new);
+
+        if(request.getDurationDays() < 1)
+            throw new IllegalArgumentException("Minimum duration of challenge must be 1 day.");
+        if(securityRepo.existsByUniqueId(request.getUniqueId()))
+            throw new IllegalArgumentException("This private challenge ID is already taken. Choose another one.");
+
+
+        Challenge challenge = ChallengeRequest.toChallenge(request);
+        challenge.setUser(user);
+        challenge.setStatus(ChallengeStatus.ACTIVE);
+
+        LocalDate today = LocalDate.now();
+        LocalDate endDate = today.plusDays(request.getDurationDays());
+        challenge.setDurationDays(request.getDurationDays());
+        challenge.setCreationDate(today);
+        challenge.setEndDate(endDate);
+
+        Challenge savedChallenge = challengeRepo.save(challenge);
+
+        if(request.getType() == ChallengeType.PRIVATE){
+
+            ChallengeSecurity security = ChallengeRequest.toChallengeSecurity(request);
+            security.setPin(passwordEncoder.encode(request.getPin()));
+            security.setChallenge(savedChallenge);
+
+            securityRepo.save(security);
+        }
+
+        UserChallenge userChallenge = new UserChallenge();
+        userChallenge.setUser(user);
+        userChallenge.setChallenge(savedChallenge);
+        userChallenge.setStreak(0);
+        userChallenge.setJoiningDate(today);
+
+        userChallengeRepo.save(userChallenge);
+    }
+
+    public void updateChallenge(Long challengeId, ChallengeRequest request){
+        Challenge challenge = challengeRepo.findById(challengeId)
+                .orElseThrow(() -> new IllegalArgumentException("Challenge Not Found!"));
+        User user = userRepo.findById(request.getUserId()).orElseThrow(UserNotFoundException::new);
+
+        if(request.getDurationDays() < 1)
+            throw new IllegalArgumentException("Minimum duration of challenge must be 1 day.");
+
+        challenge.setChallengeName(request.getChallengeName());
+        challenge.setDescription(request.getDescription());
+
+        LocalDate creationDate = challenge.getCreationDate();
+        LocalDate newEndDate = creationDate.plusDays(request.getDurationDays());
+        challenge.setDurationDays(request.getDurationDays());
+        challenge.setEndDate(newEndDate);
+
+        if(challenge.getType() == ChallengeType.PRIVATE){
+            ChallengeSecurity security = securityRepo.findByChallengeId(challenge.getId());
+            security.setPin(request.getPin());
+
+            securityRepo.save(security);
+        }
+
+        challengeRepo.save(challenge);
+    }
+
+    public List<ChallengeResponse> getAllChallenges(){
+        return challengeRepo.findAll()
+                .stream()
+                .map(ChallengeResponse::fromChallenge)
+                .toList();
+    }
+
+    public ChallengeResponse getChallenge(Long challengeId){
+        Challenge challenge = challengeRepo.findById(challengeId)
+                .orElseThrow(() -> new IllegalArgumentException("Challenge Not Found!"));
+        return ChallengeResponse.fromChallenge(challenge);
+    }
+
+    public List<ChallengeResponse> getSearchedChallenges(String searchInput){
+        return challengeRepo.findSearchedChallenges(searchInput).stream().map(ChallengeResponse::fromChallenge).toList();
+    }
+
+    public Boolean canJoinPrivateChallenge(ChallengeRequest request){
+        if(!securityRepo.existsByUniqueIdAndChallengeId(request.getUniqueId(), request.getChallengeId()))
+            throw new IllegalArgumentException("Invalid ID Or PIN.");
+
+        ChallengeSecurity security = securityRepo.findByUniqueId(request.getUniqueId());
+        return security != null && passwordEncoder.matches(request.getPin(), security.getPin());
+    }
+}
---
+++ habit_management/src/main/java/com/project/habit_management/service/UserChallengeService.java
@@ -0,0 +1,114 @@
+package com.project.habit_management.service;
+
+import com.project.habit_management.dto.ChallengeLeaderboardResponse;
+import com.project.habit_management.dto.UserChallengeRequest;
+import com.project.habit_management.dto.UserChallengeResponse;
+import com.project.habit_management.exception.UserNotFoundException;
+import com.project.habit_management.model.Challenge;
+import com.project.habit_management.model.User;
+import com.project.habit_management.model.UserChallenge;
+import com.project.habit_management.repository.ChallengeRepo;
+import com.project.habit_management.repository.UserChallengeRepo;
+import com.project.habit_management.repository.UserRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class UserChallengeService {
+    final private UserChallengeRepo userChallengeRepo;
+    final private ChallengeRepo challengeRepo;
+    final private UserRepo userRepo;
+
+    public UserChallengeService(UserChallengeRepo userChallengeRepo, ChallengeRepo challengeRepo, UserRepo userRepo) {
+        this.userChallengeRepo = userChallengeRepo;
+        this.challengeRepo = challengeRepo;
+        this.userRepo = userRepo;
+    }
+
+    public void joinChallenge(Integer userId, Long challengeId){
+        if(userChallengeRepo.existsByUserIdAndChallengeId(userId, challengeId))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "You have already joined this challenge.");
+        User user = userRepo.findById(userId).orElseThrow(() -> new UserNotFoundException());
+        Challenge challenge = challengeRepo.findById(challengeId)
+                .orElseThrow(() -> new IllegalArgumentException("Challenge Not Found!"));
+
+        UserChallenge userChallenge = new UserChallenge();
+        userChallenge.setUser(user);
+        userChallenge.setChallenge(challenge);
+        userChallenge.setStreak(0);
+        LocalDate today = LocalDate.now();
+        userChallenge.setJoiningDate(today);
+
+        userChallengeRepo.save(userChallenge);
+    }
+
+    public List<UserChallengeResponse> getAllUserChallenges(Integer userId){
+        if(!userRepo.existsById(userId))
+            throw new UserNotFoundException();
+
+        return userChallengeRepo.findAllChallengesByUserId(userId);
+    }
+
+    public UserChallengeResponse getUserChallengeDetails(Integer userId, Long challengeId){
+        if(!userChallengeRepo.existsByUserIdAndChallengeId(userId, challengeId)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Challenge details not found!");
+        }
+
+        return  userChallengeRepo.findAllChallengesByUserIdAndChallengeId(userId, challengeId);
+    }
+
+    public boolean canComplete(Integer userId, Long challengeId){
+        if(!userRepo.existsById(userId))
+            throw new UserNotFoundException();
+        if(!userChallengeRepo.existsByUserIdAndChallengeId(userId, challengeId))
+            throw new IllegalArgumentException("You have not joined this challenge yet.");
+
+        UserChallenge userChallenge = userChallengeRepo.findByUserIdAndChallengeId(userId, challengeId);
+        LocalDate today = LocalDate.now();
+
+        if(userChallenge.getLastCompleted() == null) return true;
+        return today.isAfter(userChallenge.getLastCompleted());
+    }
+
+    public String markComplete(Integer userId, Long challengeId){
+        if(!canComplete(userId, challengeId))
+            return "Challenge is already completed.";
+
+        UserChallenge userChallenge = userChallengeRepo.findByUserIdAndChallengeId(userId, challengeId);
+        LocalDate today = LocalDate.now();
+        userChallenge.setLastCompleted(today);
+
+        int streak = userChallenge.getStreak();
+        userChallenge.setStreak(++streak);
+
+        userChallengeRepo.save(userChallenge);
+        return "Challenge is completed.";
+    }
+
+    public Long countChallengeParticipants(Long challengeId){
+        return userChallengeRepo.countByChallengeId(challengeId);
+    }
+
+    public Boolean canJoin(Integer userId, Long challengeId){
+        if(userChallengeRepo.existsByUserIdAndChallengeId(userId, challengeId)){
+            return false;
+        }
+
+        return true;
+    }
+
+    public List<ChallengeLeaderboardResponse> getChallengeLeaderboard(Long challengeId){
+        if(!userChallengeRepo.existsByChallengeId(challengeId))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Challenge Not Found!");
+
+        return userChallengeRepo.findLeaderboardByChallengeId(challengeId);
+    }
+
+    public void leaveChallenge(Integer userId, Long challengeId){
+        userChallengeRepo.deleteByUserIdAndChallengeId(userId, challengeId);
+    }
+}
